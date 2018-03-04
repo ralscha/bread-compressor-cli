f443dd6060832cc362574d0705fc9c73b5df562a
Ralph Schaer <ralphschaer@gmail.com>
2018-03-04 10:59:22 +0100

    Make limit configurable

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const promiseLimit = require('promise-limit')
 const util = require('util');
 const chalk = require('chalk');
 
-const limit = promiseLimit(2)
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
@@ -21,13 +20,14 @@ function parseArgs(algorithm) {
 		.version('1.0.0')
 		.usage('[options] <globs ...>')
 		.option('-s, --stats', 'Show statistics')
-		.option('-a, --algorithm <items>', 'Comma separated list of compression algorithms. Supported values are "brotli" and "gzip" (default "brotli,gzip")', items=>items.split(','))
-		.option('-n, --no-default-ignores', 'Do not add default ignores')
-		.option('--zopfli-numiterations [value]', 'Maximum amount of times to rerun forward and backward pass to optimize LZ77 compression cost. Good values: 10, 15 for small files, 5 for files over several MB in size or it will be too slow. (default: 15)', parseInt)
-		.option('--zopfli-blocksplittinglast [value]', 'If "true", chooses the optimal block split points only after doing the iterative LZ77 compression. If "false", chooses the block split points first, then does iterative LZ77 on each individual block. If "both", first runs with false, then with true and keeps the smaller file. (default: "false")')
-		.option('--brotli-mode [value]', '0 = generic, 1 = text (default), 2 = font (WOFF2)', parseInt)
-		.option('--brotli-quality [value]', '0 - 11, (default: 11)', parseInt)
-		.option('--brotli-lgwin [value]', 'window size (default: 22)', parseInt)
+		.option('-a, --algorithm <items>', 'Comma separated list of compression algorithms. Supported values are "brotli" and "gzip". Default "brotli,gzip"', items=>items.split(','))
+		.option('-n, --no-default-ignores', 'Do not add default glob ignores')
+		.option('-l, --limit <value>', 'Number of tasks running concurrently. Default 2', parseInt)
+		.option('--zopfli-numiterations <value>', 'Maximum amount of times to rerun forward and backward pass to optimize LZ77 compression cost. Good values: 10, 15 for small files, 5 for files over several MB in size or it will be too slow. Default 15', parseInt)
+		.option('--zopfli-blocksplittinglast <value>', 'If "true", chooses the optimal block split points only after doing the iterative LZ77 compression. If "false", chooses the block split points first, then does iterative LZ77 on each individual block. If "both", first runs with false, then with true and keeps the smaller file. Default "false"')
+		.option('--brotli-mode <value>', '0 = generic, 1 = text (default), 2 = font (WOFF2)', parseInt)
+		.option('--brotli-quality <value>', '0 - 11. Default 11', parseInt)
+		.option('--brotli-lgwin <value>', 'Window size. Default 22', parseInt)
 		.parse(process.argv);
 }
 
@@ -58,6 +58,8 @@ async function compress(algorithm) {
 	const paths = globby.sync([...globs], { onlyFiles: true });
 	const start = Date.now();
 
+	const limit = promiseLimit(program.limit ? program.limit : 2);
+	
 	let results;
 	if (algorithm === 'brotli') {
 		const options = {
