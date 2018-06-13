2ba4fdd8b4c7310900129afd595a8e1c4e90fc2e
Julian Poyourow <julianpoyo@gmail.com>
2018-06-13 09:35:34 -0700

    Updated help hint for -l

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function parseArgs(algorithm) {
 		.option('-s, --stats', 'Show statistics')
 		.option('-a, --algorithm <items>', 'Comma separated list of compression algorithms. Supported values are "brotli" and "gzip". Default "brotli,gzip"', items=>items.split(','))
 		.option('-n, --no-default-ignores', 'Do not add default glob ignores')
-		.option('-l, --limit <value>', 'Number of tasks running concurrently. Default 2', parseInt)
+		.option('-l, --limit <value>', 'Number of tasks running concurrently. Default is your total number of cores', parseInt)
 		.option('--zopfli-numiterations <value>', 'Maximum amount of times to rerun forward and backward pass to optimize LZ77 compression cost. Good values: 10, 15 for small files, 5 for files over several MB in size or it will be too slow. Default 15', parseInt)
 		.option('--zopfli-blocksplittinglast <value>', 'If "true", chooses the optimal block split points only after doing the iterative LZ77 compression. If "false", chooses the block split points first, then does iterative LZ77 on each individual block. If "both", first runs with false, then with true and keeps the smaller file. Default "false"')
 		.option('--brotli-mode <value>', '0 = generic, 1 = text (default), 2 = font (WOFF2)', parseInt)
