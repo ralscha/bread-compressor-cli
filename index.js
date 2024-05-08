c1d150a53016b9d68768ea6ea38a5e7e28693652
Ralph Schaer <ralphschaer@gmail.com>
2024-05-08 07:08:15 +0200

    Bugfix for parse function. Commander passes two arguments to custom parse functions.
    Resolves #6

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import {fork} from "child_process";
 import {fileURLToPath} from 'url';
 
 function parseArgs() {
+    let intParseFunc = (n) => parseInt(n);
     if (Object.keys(program.opts()).length === 0) {
         program
             .version('3.1.0')
@@ -17,13 +18,13 @@ function parseArgs() {
             .option('-s, --stats', 'Show statistics')
             .option('-a, --algorithm <items>', 'Comma separated list of compression algorithms. Supported values are "brotli", "gzip" and "zstd". Default "brotli,gzip"', items => items.split(','))
             .option('-n, --no-default-ignores', 'Do not add default glob ignores')
-            .option('-l, --limit <value>', 'Number of tasks running concurrently. Default is your total number of cores', parseInt)
-            .option('--zopfli-numiterations <value>', 'Maximum amount of times to rerun forward and backward pass to optimize LZ77 compression cost. Good values: 10, 15 for small files, 5 for files over several MB in size or it will be too slow. Default 15', parseInt)
+            .option('-l, --limit <value>', 'Number of tasks running concurrently. Default is your total number of cores', intParseFunc)
+            .option('--zopfli-numiterations <value>', 'Maximum amount of times to rerun forward and backward pass to optimize LZ77 compression cost. Good values: 10, 15 for small files, 5 for files over several MB in size or it will be too slow. Default 15', intParseFunc)
             .option('--zopfli-blocksplittinglast <value>', 'If "true", chooses the optimal block split points only after doing the iterative LZ77 compression. If "false", chooses the block split points first, then does iterative LZ77 on each individual block. If "both", first runs with false, then with true and keeps the smaller file. Default "false"')
-            .option('--brotli-mode <value>', '0 = generic, 1 = text (default), 2 = font (WOFF2)', parseInt)
-            .option('--brotli-quality <value>', '0 - 11. Default 11', parseInt)
-            .option('--brotli-lgwin <value>', 'Window size. Default 22', parseInt)
-            .option('--zstd-level <value>', 'Zstandard compression level. Default 3', parseInt)
+            .option('--brotli-mode <value>', '0 = generic, 1 = text (default), 2 = font (WOFF2)', intParseFunc)
+            .option('--brotli-quality <value>', '0 - 11. Default 11', intParseFunc)
+            .option('--brotli-lgwin <value>', 'Window size. Default 22', intParseFunc)
+            .option('--zstd-level <value>', 'Zstandard compression level. Default 3', intParseFunc)
     }
     program.parse();
 }
